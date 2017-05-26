82c4131b0fe9e97736c7c509a4eeda007116505f
Ivan Merxbauer <ivan.merxbauer@seznam.cz>
2017-05-26 23:21:14 +0200

    PULLNI TOHLE

--- VillageMaster_master/VillageMaster/SQLiteExample/SQLiteExample/SQLiteExample/Question.cs
+++ VillageMaster_master/VillageMaster/SQLiteExample/SQLiteExample/SQLiteExample/Question.cs
@@ -7,20 +7,15 @@ using SQLite;
 
 namespace SQLiteExample
 {
-  public  class TodoItem
+  public  class Question
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string Type { get; set; }
         public string Text { get; set; }
 
-        public TodoItem()
+        public Question()
         {
-        }
-
-        public override string ToString()
-        {
-            return "ID" + ID + " Name " + Name + " Text " + Text;
         }
     }
 }
--- VillageMaster_master/VillageMaster/SQLiteExample/SQLiteExample/SQLiteExample/TodoItemDatabase.cs
+++ VillageMaster_master/VillageMaster/SQLiteExample/SQLiteExample/SQLiteExample/TodoItemDatabase.cs
@@ -15,26 +15,26 @@ namespace SQLiteExample
         public TodoItemDatabase(string dbPath)
         {
             database = new SQLiteAsyncConnection(dbPath);
-            database.CreateTableAsync<TodoItem>().Wait();
+            database.CreateTableAsync<Question>().Wait();
         }
 
 
-        public Task<List<TodoItem>> GetItemsAsync()
+        public Task<List<Question>> GetItemsAsync()
         {
-            return database.Table<TodoItem>().ToListAsync();
+            return database.Table<Question>().ToListAsync();
         }
 
-        public Task<List<TodoItem>> GetItemsNotDoneAsync()
+        public Task<List<Question>> GetItemsNotDoneAsync()
         {
-            return database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+            return database.QueryAsync<Question>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
         }
 
-        public Task<TodoItem> GetItemAsync(int id)
+        public Task<Question> GetItemAsync(int id)
         {
-            return database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+            return database.Table<Question>().Where(i => i.ID == id).FirstOrDefaultAsync();
         }
 
-        public Task<int> SaveItemAsync(TodoItem item)
+        public Task<int> SaveItemAsync(Question item)
         {
             if (item.ID != 0)
             {
@@ -46,7 +46,7 @@ namespace SQLiteExample
             }
         }
 
-        public Task<int> DeleteItemAsync(TodoItem item)
+        public Task<int> DeleteItemAsync(Question item)
         {
             return database.DeleteAsync(item);
         }
