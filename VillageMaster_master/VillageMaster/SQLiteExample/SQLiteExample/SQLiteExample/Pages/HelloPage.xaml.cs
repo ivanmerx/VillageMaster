0f56605451575fac569b8f8157f9911fc612ba28
Ivan Merxbauer <ivan.merxbauer@seznam.cz>
2017-05-26 23:21:14 +0200

    PULLNI TOHLE

--- VillageMaster_master/VillageMaster/SQLiteExample/SQLiteExample/SQLiteExample/Pages/HelloPage.xaml.cs
+++ VillageMaster_master/VillageMaster/SQLiteExample/SQLiteExample/SQLiteExample/Pages/HelloPage.xaml.cs
@@ -16,6 +16,9 @@ namespace SQLiteExample.Pages
             InitializeComponent();
             //https://www.google.cz/search?q=happy+smile+icon+drawing&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiBxpjFiIbUAhXCSBQKHVmCAK8Q_AUICigB&biw=1920&bih=925#imgrc=Jlg5R-FFTAKzWM:
             //https://thumbs.dreamstime.com/z/medieval-weapons-icons-vector-illustration-47855259.jpg
+            var obrazek = new Image { Aspect = Aspect.AspectFit };
+            obrazek.Source = ImageSource.FromResource("SQLiteExample.logo_village.png");
+            obrazek.Source = "@drawable/logo_village.png";
         }
     }
 }
